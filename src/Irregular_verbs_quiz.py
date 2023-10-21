2ef3c8884220bc60955e862f2d70623f4f65b008
AlbertoGirardi <alberto.gir747@gmail.com>
2023-10-21 12:05:36 +0200

    verbs

--- src/Irregular_verbs_quiz.py
+++ src/Irregular_verbs_quiz.py
@@ -1,3 +1,14 @@
+import pandas as pd
+
+
+
 print("QUIZ VERBI IRREGOLARI")
 
+data_file = 'data\\verbs.csv'
+
+
+lista_verbi = pd.read_csv( data_file, header = 0, index_col=0 )
+
+for verb in lista_verbi:
+    print(verb)
 
